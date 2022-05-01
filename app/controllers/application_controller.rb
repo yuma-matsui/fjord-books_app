e4e071a4bb3f98c50b3e7b1ea569a5508e139b19
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-01 11:35:25 +0900

    URLパラメータによる翻訳言語の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
