525cc5d2f148fcf7a009d020688d3f08f553ffea
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-03 07:11:56 +0900

    認可の設定#ログインユーザーのみ各サービス利用許可

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /books
   # GET /books.json
