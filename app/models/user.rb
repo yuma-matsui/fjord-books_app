12be645934531d1bf5ef0ca23d756262396e7dc2
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-04 08:26:20 +0900

    ユーザー一覧、詳細ページへの画像表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_one_attached :image
+
+  def thumbnail(size: [300, 300])
+    image.variant(resize_to_limit: size)
+  end
 end
