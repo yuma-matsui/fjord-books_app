cac0736841da43672e0e52f1ab265615304cd79f
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-04 08:41:30 +0900

    選択できる画像タイプの限定と登録情報変更画面での画像表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :image
 
   def thumbnail(size: [300, 300])
