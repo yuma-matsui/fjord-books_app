8e3355b9dd3629186c0b4b7db487f535966ba741
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-06 21:56:18 +0900

    User, FollowingFollowerモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_following, class_name: 'FollowingFollower',
+                              foreign_key: 'follower_id',
+                              dependent: :destroy
+  has_many :following, through: :active_following, source: :followed
+
+  has_many :passive_following, class_name: 'FollowingFollower',
+                               foreign_key: 'followed_id',
+                               dependent: :destroy
+  has_many :followers, through: :passive_following, source: :follower
 end
