dff513b44fdc29b33356c936914ea982a36b16b9
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-07 09:34:48 +0900

    フォロー機能用のメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,25 @@ class User < ApplicationRecord
 
   has_many :active_following, class_name: 'FollowingFollower',
                               foreign_key: 'follower_id',
+                              inverse_of: :follower,
                               dependent: :destroy
   has_many :following, through: :active_following, source: :followed
 
   has_many :passive_following, class_name: 'FollowingFollower',
                                foreign_key: 'followed_id',
+                               inverse_of: :followed,
                                dependent: :destroy
   has_many :followers, through: :passive_following, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_following.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
