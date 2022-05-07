6a4ed605b714c07cb297f56f5a1b9dda8d570454
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-08 08:35:07 +0900

    ユーザー詳細ページにフォローボタンの追加

--- app/controllers/following_followers_controller.rb
+++ app/controllers/following_followers_controller.rb
@@ -4,12 +4,12 @@ class FollowingFollowersController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to users_path
+    redirect_to user
   end
 
   def destroy
     user = FollowingFollower.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to users_path
+    redirect_to user
   end
 end
