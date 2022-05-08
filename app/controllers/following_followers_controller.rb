eedf64daa07e1a189652157e6edbc7a49b85f3ab
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-08 12:11:44 +0900

    フォロー・アンフォロー機能の非同期化

--- app/controllers/following_followers_controller.rb
+++ app/controllers/following_followers_controller.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
 class FollowingFollowersController < ApplicationController
+  after_action :return_ajax, only: %i[create destroy]
+
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = FollowingFollower.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user = FollowingFollower.find(params[:id]).followed
+    current_user.unfollow(@user)
+  end
+
+  private
+
+  def return_ajax
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
