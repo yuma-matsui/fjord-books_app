516e469a680650ccf1d71db3deef3c1ab86c9fa1
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-14 20:57:04 +0900

    Repors、Commentsの投稿者の名前ががない場合のメールアドレス表示対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def allowed_user?(user)
+    current_user == user
+  end
+
+  def show_name_or_email(user)
+    user.name.blank? ? user.email : user.name
+  end
 end
