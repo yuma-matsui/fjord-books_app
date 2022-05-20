2672220015a38e113a5f808741d4c08cd78ede1a
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-21 08:26:43 +0900

    ReportModelのインスタンスメソッド用テストの作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @report = FactoryBot.create(:report)
+    @user = FactoryBot.create(:user)
+  end
+
+  test 'user should edit own report' do
+    user = @report.user
+    assert @report.editable?(user)
+  end
+
+  test "user shouldn't edit other user's report" do
+    assert_not @report.editable?(@user)
+  end
+
+  test 'should return short version time' do
+    assert_equal @report.created_at.to_date, @report.created_on
+  end
+
+  test 'should count down Report when user is destroyed' do
+    assert_difference 'Report.count', -1 do
+      @report.user.destroy
+    end
+  end
 end
