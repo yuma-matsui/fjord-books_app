bc5ad08e4ffe3a9fff1b27099f19734ee61f3850
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-21 10:29:36 +0900

    Booksリソース用の既存システムテスト修正

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,47 +1,52 @@
 # frozen_string_literal: true
 
 require 'application_system_test_case'
+require 'helpers/sign_in_helper'
 
 class BooksTest < ApplicationSystemTestCase
+  include SignInHelper
+
   setup do
-    @book = books(:one)
+    @book = FactoryBot.create(:book)
+    user = FactoryBot.create(:user)
+    sign_in_as(user)
   end
 
   test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+    visit books_path
+    assert_selector 'td', text: @book.title
   end
 
   test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
+    visit books_path
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'メモ', with: @book.memo
+    fill_in 'タイトル', with: @book.title
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    click_on '戻る'
   end
 
   test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
+    visit books_path
+    click_on '編集'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    click_on '戻る'
   end
 
   test 'destroying a Book' do
-    visit books_url
+    visit books_path
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
