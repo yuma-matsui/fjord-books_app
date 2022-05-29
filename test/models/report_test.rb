94b41f73409da419a921c9bd51c20d4dd715ac86
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-29 10:14:38 +0900

    インスタンス変数名の変更とcreated onのテスト修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   setup do
     @report = FactoryBot.create(:report)
-    @user = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user)
   end
 
   test 'user should edit own report' do
@@ -13,11 +13,11 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test "user shouldn't edit other user's report" do
-    assert_not @report.editable?(@user)
+    assert_not @report.editable?(@other_user)
   end
 
   test 'should return short version time' do
-    assert_equal @report.created_at.to_date, @report.created_on
+    assert_equal Time.zone.today, @report.created_on
   end
 
   test 'should count down Report when user is destroyed' do
